19e143f5464b3d1e92df60c23e458b0ec990fac8
Yandrak <hectormatgon@gmail.com>
2019-11-05 21:04:04 +0100

    Añadimos el curriculum con las imagenes de los lenguajes y herramientas

--- Webs/script.js
+++ Webs/script.js
@@ -1,15 +1,11 @@
-	
-
-
+//Función para analizar el número del dni y sacar la letra correspondiente según el resto de dividirlo por 23
 function verLetra(dni){
 		//Calculo el resto de dividir entre 23
 		var letras = "TRWAGMYFPDXBNJZSQVHLCKE";
 		var resultado;
 		resultado = dni % 23;
-		var letra = letras.substring(resultado,resultado+1); //+1 para que pille todas
-		//alert("La letra de tu dni es la " + letra)
-		//console.log("La letra es " + letra);
-		if(isNaN(dni)){
+		var letra = letras.substring(resultado,resultado+1); //+1 para que pille todas las letras                 
+		if(isNaN(dni) || dni == false){
 			alert("El dni no es Numérico")
 		}
 
