90a59193481a10b67f774af67f340ac4c8f14c11
Hector <hectormatgon@gmail.com>
2019-11-21 13:51:53 +0100

    Ejercicio del palindromo y ocultar texto

--- Webs/Ejercicios en proceso/script/dom.js
+++ Webs/Ejercicios en proceso/script/dom.js
@@ -1,8 +1,18 @@
-function desaparece(){
-	document.getElementById("texto1").style.display = 'none';
-	//document.getElementById("enlace1").innnerHTML = "Nuevo texto"
+
+function mostrar_ocultar(texto, enlace){
+
+	//var texto = document.getElementById(texto.id);
+	var enlace = document.getElementById(enlace.id);
+
+	if(texto.style.display == "none"){
+		enlace.innerHTML = "Ocultar contenido";
+		document.getElementById(texto.id).style.display = "block";
+	}
+	else{
+		enlace.innerHTML = "Mostrar contenido";
+		document.getElementById(texto.id).style.display = "none";
+	}
 }
 
-function mostrar(){
-	document.getElementById("texto1").style.display = 'block';
-}
+
+
